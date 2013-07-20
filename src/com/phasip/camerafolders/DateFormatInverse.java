8efb71c00d41992373b16aea71e942bcb24927f6
Pasi Saarinen <saarinen.pasi@gmail.com>
2013-07-20 18:30:38 +0200

    Better gitignore

---
+++ src/com/phasip/camerafolders/DateFormatInverse.java
@@ -0,0 +1,16 @@
+package com.phasip.camerafolders;
+
+import java.util.Date;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.text.format.DateFormat;
+
+@TargetApi(Build.VERSION_CODES.CUPCAKE)
+public class DateFormatInverse extends DateFormat {
+	public static String invertFormat(String s, Date d) {
+		
+		return s;
+		
+	}
+}
